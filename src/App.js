274f9751f0da7b6ea8b6105263ee72f00005887a
tupm58 <xliucuxiux@gmail.com>
2023-04-25 22:20:17 +0700

    use redux use redux toolkit (createSlice, configureStore)

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Counter from './components/Counter';
+import {Fragment} from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import {useSelector} from "react-redux";
+
+
+function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
+  
+  return (
+    <Fragment>
+      <Header/>
+      {!isAuth && <Auth/>}
+      <Counter />
+
+    </Fragment>
+  );
+}
+
+export default App;
